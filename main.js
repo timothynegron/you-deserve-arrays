8b9ef453f7c5bfe494b6e16f08d5d8bd3db64747
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-01 13:13:22 -0500

    Adds functions. All test passed.

--- main.js
+++ main.js
@@ -2,13 +2,36 @@
  * YOUR CODE HERE *
  ******************/
 
+function getFirstItemFrom(arr) {
+  return arr[0];
+}
 
+function getLastItemFrom (arr) {
+  return arr[arr.length - 1];
+}
 
+function getIndex3 (arr) {
+  if (arr.length >= 4){
+    return arr[3];
+  }
+  else{
+    return arr[arr.length - 1];
+  }
+}
 
+function isLongList (arr) {
+  return arr.length >= 10;
+}
 
+function firstItemIsNumber (arr) {
+  return typeof arr[0] === 'number';
+}
 
+function secondCharOfThirdString (arr) {
+  return arr[2][1];
+}
 
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
